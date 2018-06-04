7f17f767e5e44cc4f368ddb0ccef59f02a3ee025
Michael Anthony Ballard <MichaelBallard@Michaels-MacBook-Pro.local>
2018-06-04 16:51:18 -0400

    Completed Lab

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		erb :index
+	end
+
+	get '/info' do
+		#erb :info # the name does not have to mathc the route
+		erb :info
 	end
 end
